8b707f80a2766a696017eabf37cc85df98ff29b9
vainotuisk <vainotuisk@gmail.com>
2016-12-30 16:31:50 +0200

    esimene

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+
+          <h2>Hindaja abimees</h2>
+        </div>
+        <p className="App-intro">
+<code>src/App.js</code> 
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
